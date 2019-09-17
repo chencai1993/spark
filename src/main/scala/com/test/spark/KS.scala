9d990fb56ec7c635c65c1c0d2da6bc79870395aa
chencai <690799780@qq.com>
2019-09-17 19:47:50 +0800

    start

--- src/main/scala/com/test/spark/KS.scala
+++ src/main/scala/com/test/spark/KS.scala
@@ -17,42 +17,50 @@ object KS {
     val values = data.filter(data.col("index").isin(pos:_*)).select(col).collect().toArray.map(line=>line(0).asInstanceOf[Double])
     values
   }
-
+  def getCount(df:DataFrame):Map[Int,Long]={
+    var group = df.select("label").groupBy("label").count().collect().toArray.map(line=>(line(0).toString.toInt,line(1).toString.toLong))
+    var res = Map[Int,Long](1->0,0->0)
+    group.foreach((r)=>res +=(r._1->r._2))
+    res
+  }
   def getSeq(parts:Array[Double],index:Int,seq_good:Long,seq_bad:Long,total_good:Long,total_bad:Long,none_total_good:Long,none_total_bad:Long): Array[Double]=
   {
     var res = List[Double]()
-    /*
-    res:+=index
-    res:+=parts(index-1)
-    res:+=parts(index)
-    res:+=seq_bad+seq_good
-    res:+=seq_bad
-    res:+=seq_good
+    res:+=index.toDouble
+    res:+=parts(index-1).toDouble
+    res:+=parts(index).toDouble
+    res:+=(seq_bad+seq_good).toDouble
+    res:+=seq_bad.toDouble
+    res:+=seq_good.toDouble
     res:+=(seq_good+seq_bad)*1.0/(total_good+total_bad)
     res:+=(seq_bad)*1.0/(seq_good+seq_bad)
-   */
     res.toArray
   }
-  def KS(df:DataFrame,col:String):Map[String,Any]={
+  def KS(df:DataFrame,col:String):List[Array[Double]]={
     var map = Map[String,Any]()
     var data = Utils.tsCols(df)
     var colName = Utils.tsCols(col)
     var none_data = data.filter(data(colName).isNull)
-    data = data.filter(data(colName).isNotNull).sort(colName)
+    data = data.filter(data(colName).isNotNull)
     data = data.select("label",colName).sort(colName)
-    var Seq(total_good,total_bad)=Seq(data.filter(data("label")===0).count(),data.filter(data("label")===1).count())
-    var Seq(none_total_good,none_total_bad)=Seq(none_data.filter(data("label")===0).count(),none_data.filter(data("label")===1).count())
+    var count = getCount(data)
+    var Seq(total_good,total_bad)=Seq(count.get(0).get,count.get(1).get)
+    count = getCount(none_data)
+    var Seq(none_total_good,none_total_bad)=Seq(count.get(0).get,count.get(1).get)
     data = Utils.add_index(data)
     var parts = cut(data,10,colName)
+    var res = List[Array[Double]]()
     for(index<-Range(1,parts.length)){
+      var t = data.filter(data(col)>parts(index-1) && data(col)<parts(index))
       if(index==1){
-        var t = data.filter(data(col)>=parts(index-1) && data(col)<parts(index))
-        val seq_good = t.filter(t("label")===0).count()
-        val seq_bad = t.filter(t("label")===1).count()
-        val seq_res = getSeq(parts,index,seq_good,seq_bad,total_good,total_bad,none_total_good ,none_total_bad)
+        t = data.filter(data(col)>=parts(index-1) && data(col)<parts(index))
       }
+      count = getCount(t)
+      var Seq(seq_good,seq_bad)=Seq(count.get(0).get,count.get(1).get)
+      val seq_res = getSeq(parts,index,seq_good,seq_bad,total_good,total_bad,none_total_good ,none_total_bad)
+      res:+=seq_res
     }
-    map
+    res
   }
 
   import org.apache.spark.sql.functions.udf
@@ -61,8 +69,10 @@ object KS {
 
     val path = "scala_test"
     var df = Utils.read(path)
-    val col = "shaohua.inop.all.v01.score"
-    KS(df,col)
+    val col = Utils.tsCols("shaohua.inop.all.v01.score")
+    var r = KS(df,col)
+    println(r)
+
   }
 
 
